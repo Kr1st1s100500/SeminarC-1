13f6248f62d81bce68fb4c3e0e309e6006bae707
Кристина <117847089+Kr1st1s100500@users.noreply.github.com>
2023-03-12 16:58:42 +0300

    Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который
+// 1. принимает на вход два числа (A и B) и 
+// 2. возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = Exponentiation(numberA, numberB);
+Console.WriteLine($"{result}");
+
+int Exponentiation(int numA, int numB)
+{
+    int res = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        res = res * numA;
+    }
+    return res;
+}
+
+
---
+++ Task29/Program.cs
@@ -0,0 +1,43 @@
+﻿//  Напишите программу, которая 
+//  1. задаёт массив из 8 элементов и 
+//  2. выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+
+void FillArray(int[] arr)
+{   
+    Random rmd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rmd.Next(0, 100);
+
+    }
+}
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {   
+    if(i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+
+}
+void PrintArray1(int[] arr)
+{
+    Console.Write($" -> [ ");
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {   
+   if(i < arr.Length - 1) Console.Write($"{arr[i]},");
+        else Console.Write($"{arr[i]}");
+    }
+     Console.Write($" ]");
+
+}
+
+FillArray(array);
+PrintArray(array);
+PrintArray1(array);
