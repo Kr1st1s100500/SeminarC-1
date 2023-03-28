25aa182dddb167a5ec09aeb196fb41e1f7d719c3
Кристина <117847089+Kr1st1s100500@users.noreply.github.com>
2023-03-28 21:05:25 +0300

    Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. 
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] CreateArrayIntUser(int size)
+{
+    int[] arr = new int[size];
+    Console.WriteLine($"Введите {size} чисел:");
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int CountPositiveElements(int[] arr)
+{
+    int countPositiveElements = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) countPositiveElements++;
+    }
+    return countPositiveElements;
+}
+
+Console.Write("Введите количество чисел: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArrayIntUser(lenght);
+int count = CountPositiveElements(array);
+Console.WriteLine($"Количество введенных положительных чисел: {count}");
---
+++ Task54/Program.cs
@@ -0,0 +1,71 @@
+﻿//  Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortRowDesc(int[,] matrix, int row)
+{
+    bool isNotSorted = true;
+    while (isNotSorted)
+    {
+        isNotSorted = false;
+        for (int i = 1; i < matrix.GetLength(1); i++)
+        {
+            if (matrix[row, i] > matrix[row, i - 1])
+            {
+                int temp = matrix[row, i];
+                matrix[row, i] = matrix[row, i - 1];
+                matrix[row, i - 1] = temp;
+                isNotSorted = true;
+            }
+        }
+    }
+}
+
+void SortAllRowsDesc(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        SortRowDesc(matrix, i);
+    }
+}
+
+int[,] arr2d = CreateMatrixRndInt(3, 4, -9, 9);
+Console.WriteLine("Заданный массив:");
+PrintMatrix(arr2d);
+SortAllRowsDesc(arr2d);
+Console.WriteLine("Массив упорядоченный по убыванию:");
+PrintMatrix(arr2d);
